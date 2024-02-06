9b345ce535d4061eca2d87d71788ddf1ccdf4f2c
yeon <test2@test2.com>
2024-02-06 15:17:24 +0900

    timeattack01

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import "./App.css";
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { useState } from 'react';
+import { plusNum, minusNum } from './redux/modules/calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
-      <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-      </div>
-      <hr />
-      <div>
-        <h3>결과</h3>
-        <p>0</p>
-      </div>
-    </div>
-  );
+	const dispatch = useDispatch();
+
+	const data = useSelector((state) => state.calculator.number);
+
+	const [number, setNumber] = useState(0);
+
+	const onNumberChangeHandler = (e) => {
+		const { value } = e.target;
+		setNumber(+value);
+	};
+
+	const onPlusNumberHandler = () => {
+		dispatch(plusNum(number));
+		setNumber(0);
+	};
+
+	const onMinusNumberHandler = () => {
+		dispatch(minusNum(number));
+		setNumber(0);
+	};
+
+	return (
+		<div className="App">
+			<h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
+			<div>
+				<input type="number" onChange={onNumberChangeHandler}></input>
+				<button onClick={onPlusNumberHandler}>더할게요</button>
+				<button onClick={onMinusNumberHandler}>뺄게요</button>
+			</div>
+			<hr />
+			<div>
+				<h3>결과:{data}</h3>
+				<p>0</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
